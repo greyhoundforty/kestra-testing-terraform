3602c04d05553fda78888e3bd6e7bea0f2572eec
Ryan Tiffany <ryantiffany@fastmail.com>
2024-10-01 05:00:43 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,40 @@
+data "ibm_is_zones" "regional" {
+  region = var.ibmcloud_region
+}
+
+locals {
+  zones       = length(data.ibm_is_zones.regional.zones)
+  vpc_zones = {
+    for zone in range(local.zones) : zone => {
+      zone = "${var.ibmcloud_region}-${zone + 1}"
+    }
+  }
+
+  tags = [
+    "provider:ibm",
+    "region:${var.ibmcloud_region}"
+  ]
+}
+
+resource "random_string" "prefix" {
+  length  = 4
+  special = false
+  numeric = false
+  upper   = false
+}
+
+resource "tailscale_tailnet_key" "lab" {
+  reusable      = false
+  ephemeral     = true
+  preauthorized = true
+  expiry        = 7776000
+  description   = "Demo tailscale key for lab"
+}
+
+# If an existing resource group is provided, this module returns the ID, otherwise it creates a new one and returns the ID
+module "resource_group" {
+  source                       = "terraform-ibm-modules/resource-group/ibm"
+  version                      = "1.1.5"
+  resource_group_name          = var.existing_resource_group == null ? "${local.prefix}-resource-group" : null
+  existing_resource_group_name = var.existing_resource_group
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ibmcloud_region" {
+  description = "IBM Cloud region"
+  type        = string
+}
+
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API key"
+  type        = string
+}
+
+variable "existing_resource_group" {
+  description = "IBM Cloud resource group"
+  type        = string
+}
